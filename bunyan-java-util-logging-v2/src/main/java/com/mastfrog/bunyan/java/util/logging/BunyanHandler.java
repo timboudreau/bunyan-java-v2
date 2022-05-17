62bcdf9ce4bdb06905fa0a3b883c8630d638a8b4
Tim Boudreau <tim@timboudreau.com>
2022-05-17 06:01:22 -0400

    Port old jul handler to bunyan v2

---
+++ bunyan-java-util-logging-v2/src/main/java/com/mastfrog/bunyan/java/util/logging/BunyanHandler.java
@@ -0,0 +1,121 @@
+package com.mastfrog.bunyan.java.util.logging;
+
+import com.mastfrog.bunyan.java.v2.Log;
+import com.mastfrog.bunyan.java.v2.Logs;
+import com.mastfrog.util.thread.AtomicLinkedQueue;
+import java.text.MessageFormat;
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+/**
+ *
+ * @author Tim Boudreau
+ */
+public class BunyanHandler extends Handler {
+
+    private final AtomicReference<Consumer<LogRecord>> sink = new AtomicReference<>(new Queuer());
+
+    @SuppressWarnings("LeakingThisInConstructor")
+    public BunyanHandler() {
+        HandlerRegistry.register(this);
+    }
+
+    void setLoggers(Logs loggers) {
+        Consumer<LogRecord> nue = loggers == null ? new Queuer() : new Sender(loggers);
+        Consumer<LogRecord> old = sink.getAndSet(nue);
+        if (old instanceof Queuer) {
+            Queuer q = (Queuer) old;
+            for (LogRecord evt : q.drain()) {
+                nue.accept(evt);
+            }
+        }
+    }
+
+    @Override
+    public void publish(LogRecord record) {
+        sink.get().accept(record);
+    }
+
+    @Override
+    public void flush() {
+        // do nothing
+    }
+
+    @Override
+    public void close() {
+        sink.getAndSet(new NoOp());
+    }
+
+    private final class Queuer implements Consumer<LogRecord> {
+
+        private final AtomicLinkedQueue<LogRecord> cache = new AtomicLinkedQueue<>();
+
+        @Override
+        public void accept(LogRecord t) {
+            cache.add(t);
+        }
+
+        List<LogRecord> drain() {
+            return cache.drain();
+        }
+    }
+
+    private final class Sender implements Consumer<LogRecord> {
+
+        private final Logs loggers;
+
+        public Sender(Logs loggers) {
+            this.loggers = loggers;
+        }
+
+        private Log logFor(LogRecord record) {
+            Level level = record.getLevel();
+            switch (level.intValue()) {
+                case 1000:
+                    return loggers.error();
+                case 900:
+                    return loggers.warn();
+                case 800:
+                    return loggers.info();
+                default:
+                    if (level.intValue() > 1000) {
+                        return loggers.fatal();
+                    }
+                    return loggers.debug();
+            }
+        }
+
+        @Override
+        public void accept(LogRecord t) {
+            try ( Log log = logFor(t).add("logger", t.getLoggerName())) {
+                log.add("at", Instant.ofEpochMilli(t.getMillis()));
+                Object[] params = t.getParameters();
+                if (params != null && params.length > 0) {
+                    log.add("params", params);
+                }
+                if (params != null) {
+                    log.add(MessageFormat.format(t.getMessage(), params));
+                } else {
+                    log.add(t.getMessage());
+                }
+                if (t.getThrown() != null) {
+                    log.add(t.getThrown());
+                }
+            }
+        }
+    }
+
+    private static final class NoOp implements Consumer<LogRecord> {
+
+        @Override
+        public void accept(LogRecord t) {
+            // do nothing
+        }
+    }
+
+}
---
+++ bunyan-java-util-logging-v2/src/test/java/com/mastfrog/bunyan/java/util/logging/BunyanHandlerTest.java
@@ -0,0 +1,138 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Tim Boudreau.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.mastfrog.bunyan.java.util.logging;
+
+import com.google.inject.AbstractModule;
+import com.mastfrog.bunyan.java.util.logging.BunyanHandlerTest.CustomSinkModule;
+import com.mastfrog.bunyan.java.v2.JSONContext;
+import com.mastfrog.bunyan.java.v2.LogSink;
+import com.mastfrog.bunyan.java.v2.LoggingConfig;
+import com.mastfrog.bunyan.java.v2.Logs;
+import com.mastfrog.giulius.bunyan.java.v2.LoggingModule;
+import com.mastfrog.giulius.tests.GuiceRunner;
+import com.mastfrog.giulius.tests.TestWith;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author Tim Boudreau
+ */
+@RunWith(GuiceRunner.class)
+@TestWith({BunyanJavaLoggingModule.class, CustomSinkModule.class})
+public class BunyanHandlerTest {
+
+    static Logger LOGGER = Logger.getLogger("foo.bar.baz");
+    static final int FATAL_LEVEL = 60;
+    static final int ERROR_LEVEL = 50;
+    static final int WARN_LEVEL = 40;
+    static final int INFO_LEVEL = 30;
+    static final int DEBUG_LEVEL = 20;
+    static final int TRACE_LEVEL = 10;
+
+    @Test
+    public void testSomeMethod(CSink sink) {
+        Exception ex = new IllegalStateException();
+        LOGGER.log(Level.WARNING, "This is a warning");
+        LOGGER.log(Level.INFO, "This is info");
+        LOGGER.log(Level.SEVERE, "Something thrown", ex);
+
+        sink.assertHasMessage(WARN_LEVEL, "This is a warning");
+        sink.assertHasMessage(INFO_LEVEL, "This is info");
+        sink.assertHasMessage(ERROR_LEVEL, "Something thrown");
+    }
+
+    static final class CustomSinkModule extends AbstractModule {
+
+        @Override
+        protected void configure() {
+            CSink sink = new CSink();
+            LoggingConfig cfig = new LoggingConfig.Builder()
+                    .logTo(sink)
+                    .hostNameForLogRecords("localhost")
+                    .build();
+            bind(LogSink.class).toInstance(sink);
+            bind(CSink.class).toInstance(sink);
+            bind(LoggingConfig.class).toInstance(cfig);
+            install(new LoggingModule().dontBindLoggingConfig());
+            bind(Logs.class).toProvider(() -> {
+                return Logs.named("stuff");
+            });
+        }
+    }
+
+    private static final class CSink implements LogSink {
+
+        private final List<Entry> entries = new LinkedList<>();
+
+        Entry assertHasMessage(Integer level, String msg) {
+            boolean found = false;
+            Entry result = null;
+            for (Entry e : entries) {
+                if (Objects.equals(e.level, level)) {
+                    Object o = e.record.get("msg");
+                    if (o != null) {
+                        if (msg.equals(o.toString())) {
+                            found = true;
+                            result = e;
+                            break;
+                        }
+                    }
+                }
+            }
+            assertTrue("Did not find '" + msg + "' at '" + level + "' in " + entries, found);
+            return result;
+        }
+
+        @Override
+        public void push(JSONContext ctx, Map<String, Object> logrecord) {
+            Integer level = (Integer) logrecord.get("level");
+            assertNotNull(level);
+            entries.add(new Entry(level, logrecord));
+        }
+    }
+
+    static final class Entry {
+
+        private final Integer level;
+        private final Map<String, Object> record;
+
+        public Entry(Integer level, Map<String, Object> record) {
+            this.level = level;
+            this.record = record;
+        }
+
+        public String toString() {
+            return level + ": " + record;
+        }
+    }
+}
